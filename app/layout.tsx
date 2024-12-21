84b56c199596db5a55f6ba0b51e656277866ae86
Ikram Hasan <ikramhasan.dev@gmail.com>
2024-12-21 11:46:23 +0600

    add: theme toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,16 @@
-import './globals.css';
-import type { Metadata } from 'next';
-import { Play } from 'next/font/google';
+import { ThemeProvider } from "@/components/ui/theme-provider";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Play } from "next/font/google";
 
 const play = Play({
-  subsets: ['latin'],
-  weight: ['400'],
+  subsets: ["latin"],
+  weight: ["400"],
 });
 
 export const metadata: Metadata = {
-  title: 'Impossible Tic Tac Toe',
-  description: 'A game of Tic Tac Toe that is impossible to win.',
+  title: "Impossible Tic Tac Toe",
+  description: "A game of Tic Tac Toe that is impossible to win.",
 };
 
 export default function RootLayout({
@@ -19,7 +20,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={play.className}>{children}</body>
+      <body className={play.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 import { GameBoard } from '@/components/GameBoard';
 import FlickeringGrid from "@/components/ui/flickering-grid";
+import { ModeToggle } from "@/components/ui/theme-toggle";
+
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center p-4 relative">
+      <div className="absolute top-4 right-4 z-20">
+        <ModeToggle />
+      </div>
       <FlickeringGrid
         className="z-[-1] absolute inset-0 size-full"
         squareSize={4}
